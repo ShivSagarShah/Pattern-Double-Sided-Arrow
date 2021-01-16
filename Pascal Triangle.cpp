890e43ac36e04be43d8e3235dafbc6c255732832
ShivSagarShah <71564583+ShivSagarShah@users.noreply.github.com>
2021-01-17 02:21:19 +0530

    Create Pascal Triangle.cpp

---
+++ Pascal Triangle.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 0; i<n; i++)
+    {
+        int s = n-i-1;
+        while(s>0)
+        {
+            cout<<" ";
+            s--;
+        }
+        int c = 1;
+        cout<<c<<" ";
+        for(int j = 1; j<=i; j++)
+        {
+            c = c * (i-j+1)/j;
+            cout<<c<<" ";
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
