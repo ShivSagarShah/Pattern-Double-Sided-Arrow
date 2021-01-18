92743e71b465159af96af94118dc802ce20f9c96
ShivSagarShah <71564583+ShivSagarShah@users.noreply.github.com>
2021-01-18 09:34:20 +0530

    Create Swastik Pattern.cpp

---
+++ Swastik Pattern.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 1; i<n/2; i++)
+    {
+        for(int j = 1; j<=n; j++)
+        {
+            if(i == 1)
+            {
+                if(j == 1 || j>= ((n/2)+1))
+                cout<<"*";
+                else
+                cout<<" ";                             
+                
+            }
+            else
+            {
+                if(j == 1 || j == ((n/2)+1))
+                cout<<"*";
+                else
+                cout<<" ";
+                             
+            }
+            
+        }
+       
+        cout<<endl;
+    }
+    int p = n;
+    while(p--)
+    cout<<"*";
+    cout<<endl;
+    for(int i = ((n/2)+2); i<=n; i++)
+    {
+        for(int j = 1; j<=n; j++)
+        {
+            if(i != n)
+            {
+                if(j == ((n/2)+1) || j == n)
+                cout<<"*";
+                else
+                cout<<" ";                             
+                
+            }
+            else
+            {
+                if(j <= ((n/2)+1) || j == n)
+                cout<<"*";
+                else
+                cout<<" ";
+                             
+            }
+            
+        }
+       
+        cout<<endl;
+    }
+
+    
+    return 0;
+}
